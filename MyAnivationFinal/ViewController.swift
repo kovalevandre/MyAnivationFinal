d076b7713148ba3b1916277d36423b4131add8a8
Andrey <akovalev@me.com>
2023-11-11 15:07:44 +0300

    Initial Commit

---
+++ MyAnivationFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAnivationFinal
+//
+//  Created by Andrey Kovalev on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
